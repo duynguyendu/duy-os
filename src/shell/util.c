b6b3ab9fde0bf24b8ba4e0e172a9ae1f40b013ce
Duy <nguyenduduy233@gmail.com>
2021-10-16 00:58:08 +0700

    Fix `#ifndef` in util.c
    My mistake for making such a stupid thing

--- src/shell/util.c
+++ src/shell/util.c
@@ -1,7 +1,3 @@
-#ifndef __UTIL__
-#define __UTIL__
-
 #include "util.h"
 
 int equal(char *str1, char *str2) { return !strcmp(str1, str2); }
-#endif /* !__UTIL__ */
--- src/shell/util.h
+++ src/shell/util.h
@@ -1,9 +1,15 @@
+#ifndef __UTIL__
+#define __UTIL__
+
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int equal(char *str1, char *str2);
+
+#endif // !__UTIL__
