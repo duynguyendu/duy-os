619e129a75c3512f6a991ba9399b7decf3e9b010
Duy <nguyenduduy233@gmail.com>
2021-10-19 15:51:46 +0700

    lib+include: Change uint8_t* to void*

--- include/memory.h
+++ include/memory.h
@@ -2,7 +2,7 @@
 #define __MEMORY_H__
 
 #include <types.h>
-void memset(uint8_t *addr, uint8_t value, uint32_t len);
-void memcpy(uint8_t *dest, uint8_t *src, uint32_t len);
+void memset(void *addr, uint8_t value, uint32_t len);
+void memcpy(void *dest, void *src, uint32_t len);
 
 #endif // !__MEMORY_H__
--- lib/memory.c
+++ lib/memory.c
@@ -1,14 +1,16 @@
 #include <memory.h>
 
-void memset(uint8_t *addr, uint8_t value, uint32_t len) {
-    uint8_t *temp = addr;
-    for (; len != 0; len--) {
-        *temp++ = value;
+void memset(void *addr, uint8_t value, uint32_t len) {
+    char *a = addr;
+    for (uint32_t i = 0; i < len; i++) {
+        a[i] = value;
     }
 }
 
-void memcpy(uint8_t *dest, uint8_t *src, uint32_t len) {
-    for (uint32_t i = 0; i < len; i++) {
-        *(dest + i) = *(src + i);
+void memcpy(void *dest, void *src, uint32_t len) {
+    char *d = dest;
+    const char *s = src;
+    for (uint32_t i = 0 ; i < len; i++) {
+        d[i] = s[i];
     }
 }
