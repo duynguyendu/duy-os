1178bb67b738dbf26a1411fd45bfcda38f9735c7
Duy <nguyenduduy233@gmail.com>
2021-10-31 15:38:51 +0700

    interrupt: Move interrupt outside of boot dir

--- kernel/arch/i386/interrupt/idt.c
+++ kernel/arch/i386/interrupt/idt.c
@@ -3,13 +3,12 @@
 #include <isr.h>
 #include <stdio.h>
 
-
 /* Allocate idt on static memory */
 idt_entry_t idt_entry[256];
 idt_ptr_t idt_prt;
 
 /* Initialize idt */
-void init_idt() {
+void idt_init() {
     /* Set pointer to table */
     idt_prt.limit = sizeof(idt_entry_t) * 256 - 1;
     idt_prt.base = (uint32_t)&idt_entry;
@@ -17,14 +16,14 @@ void init_idt() {
     install_isr();
 
     /* Tell system location of IDT */
-    __asm__ __volatile__("lidtl (%0)" : : "r"(&idt_prt));
+    __asm__ __volatile__("lidt (%0)" : : "r"(&idt_prt));
     /* Enable interrupt */
     __asm__ __volatile__("sti");
 }
 
 /* Set each entry in idt */
 void idt_set_gate(uint8_t num, uint32_t base, uint16_t selector,
-                         uint8_t flags) {
+                  uint8_t flags) {
     idt_entry[num].base_low = base & 0xFFFF;
     idt_entry[num].base_high = (base >> 16) & 0xFFFF;
     idt_entry[num].selector = selector;
